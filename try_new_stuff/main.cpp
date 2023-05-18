cc65b9cb924a128838a524c462fcecb77de11a91
Dario Baum <dario-cbr@hotmail.de>
2023-05-19 00:24:45 +0200

    Building divide-and-conquer based double structure removal

--- try_new_stuff/main.cpp
+++ try_new_stuff/main.cpp
@@ -5,40 +5,82 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <numeric>
 
 
 
-#define TOL 0.0001
+#define TOL 0.00005
+#define EQ_DBL_DELTA 1E-16
+
+
+
+struct cmp{
+    cmp(std::vector<double> vec) : val(vec[0]){}
+    inline bool operator()(std::vector<double>& x){
+        return (fabs(x[0]-val) < EQ_DBL_DELTA);
+    }
+private:
+    double val;
+};
 
 
 
 int dac(std::vector<double> arr, double search, int l, int r){
-        int m;
-        while (l <= r){
-                m = l + (r - l)/2;
-                if (fabs(search - arr[m]) < TOL){
-                        return 1;
-                }
-                else if (search < arr[m]){
-                        r = m - 1;
-                }
-                else{
-                        l = m + 1;
-                }
+    int m;
+    while (l <= r){
+        m = l + (r - l)/2;
+        if (fabs(search - arr[m]) < TOL){
+            return 1;
         }
-        return 0;
+        else if (search < arr[m]){
+            r = m - 1;
+        }
+        else{
+            l = m + 1;
+        }
+    }
+    return 0;
+}
+
+
+
+int binary_search(std::vector<std::vector<double>> container, double val, int l, int r){
+    int m;
+    while (l <= r){
+        m = l + (r - l)/2;
+        if (fabs(val - container[m][0]) < TOL){
+            //return container[m][1];
+            return 1;
+        }
+        else if (val < container[m][0]){
+            r = m - 1;
+        }
+        else{
+            l = m + 1;
+        }
+    }
+    //return -1;
+    return 0;
+}
+
+
+
+bool sort_func(std::vector<double> a, std::vector<double> b){
+    return (a[0] < b[0]);
 }
 
 
 
 int main(int argc, char **argv){
+	int i, j;
 	int dummy;
-	double energy;
+	int n_confs = 97;
+    double energy;
 	std::string filepath;
 	std::string line;
-	int i;
-	int n_confs = 97;
 	std::vector<double> energies;
+    std::vector<double> copy_energies;
+    std::vector<std::vector<double>> container;
 	
 	for (i = 0; i < n_confs; i++){
 		filepath = "opt_dir" + std::to_string(i) + "/uffenergy";
@@ -49,27 +91,39 @@ int main(int argc, char **argv){
 			std::stringstream linestream(line);
 			linestream >> dummy >> energy;
 			energies.push_back(energy);
+            container.push_back({energy, (double)i});
 		}
 	}
 
-	/*for (i = 0; i < energies.size(); i++){
-		std::cout << energies[i] << std::endl;
-	}
-	std::cout << std::endl;
-
 	std::sort(energies.begin(), energies.end());
-
-	for (i = 0; i < energies.size(); i++){
-                std::cout << energies[i] << std::endl;
-        }
-	std::cout << std::endl;*/
+    std::sort(container.begin(), container.end(), sort_func);
 
     int counter = 0;
-	for (double energy: energies){
-		if (dac(energies, energy, 0, energies.size())){
+    std::vector<double>::iterator it1;
+    for (double energy: energies){
+        copy_energies = energies;
+        //it = std::find_if(copy_energies.begin(), copy_energies.end(), dbl_cmp(energy));
+        it1 = std::find(copy_energies.begin(), copy_energies.end(), energy);
+        copy_energies.erase(it1);
+        if (dac(copy_energies, energy, 0, copy_energies.size())){
             counter++;
         }
-	}
+    }
+    std::cout << "Von " << n_confs << " sind " << counter << " Doubles." << std::endl;
+    counter = 0;
+    std::vector<std::vector<double>> copy_container;
+    std::vector<std::vector<double>>::iterator it2;
+    std::vector<double> item;
+    for (i = 0; i < n_confs; i++){
+        copy_container = container;
+        item = copy_container[i];
+        energy = item[0];
+        it2 = std::find_if(copy_container.begin(), copy_container.end(), cmp(item));
+        copy_container.erase(it2);
+        if (binary_search(copy_container, energy, 0, copy_container.size()-1)){
+            counter++;
+        }
+    }
     std::cout << "Von " << n_confs << " sind " << counter << " Doubles." << std::endl;
 
 	return 0;

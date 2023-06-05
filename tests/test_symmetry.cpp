c6ab52ef0d44c637d214605b8f748930597f378c
Dario Baum <dario-cbr@hotmail.de>
2023-06-05 16:38:29 +0200

    Added tests for Symmetry class

--- tests/test_symmetry.cpp
+++ tests/test_symmetry.cpp
@@ -9,7 +9,8 @@
 #include <stdlib.h>
 
 
-TEST(SymmetryTest, DetectRotationalSymmetry)
+
+TEST(TestSymmetry, DetectRotationalSymmetry)
 {   
     int i;
 
@@ -25,9 +26,6 @@ TEST(SymmetryTest, DetectRotationalSymmetry)
     std::vector<int> tyrosin_atoms;
     std::vector<int> tyrosin_ring_atoms;
 
-    /*std::string alanin_file  = "/revSUPRA/inputfiles/Alanin.xyz";
-    std::string benzol_file  = "/revSUPRA/inputfiles/Benzol.xyz";
-    std::string tyrosin_file = "/revSUPRA/inputfiles/Tyrosin.xyz";*/
     std::string home_path = getenv("HOME");
     std::string alanin_file = home_path + "/revSUPRA/inputfiles/D-Alanin.xyz";
     std::string benzol_file = home_path + "/revSUPRA/inputfiles/Benzol.xyz";
@@ -63,4 +61,47 @@ TEST(SymmetryTest, DetectRotationalSymmetry)
 
     EXPECT_EQ(Sym.rot_sym_along_bond(Tyrosin_ptr, tyrosin_atoms, 13, 16, 3), false);
     EXPECT_EQ(Sym.rot_sym_along_bond(Tyrosin_ptr, tyrosin_ring_atoms, 4, 12, 2), true);
+}
+
+
+
+TEST(TestSymmetry, DetectRotationalSymmetryOrder)
+{
+    int i;
+
+    Symmetry Sym;
+
+    std::shared_ptr<Structure> Alanin_ptr  = std::make_shared<Structure>();
+    std::shared_ptr<Structure> Benzol_ptr  = std::make_shared<Structure>();
+    std::shared_ptr<Structure> Tyrosin_ptr = std::make_shared<Structure>();
+
+    std::vector<int> alanin_atoms;
+    std::vector<int> alanin_methyl_atoms;
+    std::vector<int> benzol_atoms;
+    std::vector<int> tyrosin_atoms;
+    std::vector<int> tyrosin_ring_atoms;
+
+    std::string home_path = getenv("HOME");
+    std::string alanin_file = home_path + "/revSUPRA/inputfiles/D-Alanin.xyz";
+    std::string tyrosin_file = home_path + "/revSUPRA/inputfiles/Tyrosin.xyz";
+
+    Alanin_ptr->read_xyz(alanin_file);
+    for (i = 0; i < Alanin_ptr->n_atoms; i++){
+        alanin_atoms.push_back(i);
+    }
+    alanin_methyl_atoms = {2, 3, 4, 5};
+
+    EXPECT_EQ(Sym.rot_sym_along_bond(Alanin_ptr, alanin_atoms, 0, 1), 1);
+    EXPECT_EQ(Sym.rot_sym_along_bond(Alanin_ptr, alanin_atoms, 1, 0), 1);
+    EXPECT_EQ(Sym.rot_sym_along_bond(Alanin_ptr, alanin_methyl_atoms, 0, 2), 3);
+    EXPECT_EQ(Sym.rot_sym_along_bond(Alanin_ptr, alanin_methyl_atoms, 2, 0), 3);
+
+    Tyrosin_ptr->read_xyz(tyrosin_file);
+    for (i = 0; i < Tyrosin_ptr->n_atoms; i++){
+        tyrosin_atoms.push_back(i);
+    }
+    tyrosin_ring_atoms = {0, 1, 2, 3, 4, 5};
+
+    EXPECT_EQ(Sym.rot_sym_along_bond(Tyrosin_ptr, tyrosin_atoms, 13, 16), 1);
+    EXPECT_EQ(Sym.rot_sym_along_bond(Tyrosin_ptr, tyrosin_ring_atoms, 4, 12), 2);
 }

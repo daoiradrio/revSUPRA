fe8bc48bb14ecdce1e57cf8b900a52515823babf
Dario Baum <dario-cbr@hotmail.de>
2023-06-12 16:20:34 +0200

    Removed system dependent build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baum/revSUPRA
-# Build directory: /home/baum/revSUPRA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/baum/revSUPRA/build/runtests[1]_include.cmake")
-subdirs("_deps/googletest-build")
